76cb28ab53bdfe039ab75c7ccf4763b49bc21f60
realmatic <admin@realmatic.com>
2023-08-06 17:02:36 -0700

    23app

--- import.sh
+++
@@ -1,6 +0,0 @@
-#wget --no-check-certificate -O index.html https://local.michelle.kara-pace.com/ 
-
-wget --no-check-certificate -O page/21-12-31-make-Current.html https://local.michelle.kara-pace.com/page/21-12-31-make-Current/ 
-wget --no-check-certificate -O page/21-12-31-work-Current.html https://local.michelle.kara-pace.com/page/21-12-31-work-Current/ 
-
-
---
+++ release.sh
@@ -0,0 +1,22 @@
+# set DEBUG false, production ASSET URL 
+
+# wget --no-check-certificate -O index.html $MICHELLE 
+
+# wget --no-check-certificate -O page/work.html $MICHELLE/page/work
+# wget --no-check-certificate -O page/process.html $MICHELLE/page/process
+# wget --no-check-certificate -O page/biocv.html $MICHELLE/page/biocv
+
+# wget --no-check-certificate -O page/21-12-31-make-Current.html $MICHELLE/page/21-12-31-make-Current 
+# wget --no-check-certificate -O page/21-12-31-work-Current.html $MICHELLE/page/21-12-31-work-Current 
+# wget --no-check-certificate -O page/22-12-13-make-Vigor.html $MICHELLE/page/22-12-13-make-Vigor 
+# wget --no-check-certificate -O page/22-12-13-work-Vigor.html $MICHELLE/page/22-12-13-work-Vigor 
+# wget --no-check-certificate -O page/23-03-15-make-Occupation.html $MICHELLE/page/23-03-15-make-Occupation 
+# wget --no-check-certificate -O page/23-03-15-work-Occupation.html $MICHELLE/page/23-03-15-work-Occupation 
+# wget --no-check-certificate -O page/23-05-01-make-Deliberate.html $MICHELLE/page/23-05-01-make-Deliberate 
+# wget --no-check-certificate -O page/23-05-01-work-Deliberate.html $MICHELLE/page/23-05-01-work-Deliberate 
+
+wget --no-check-certificate -O page/23app.html $MICHELLE/page/23app
+wget --no-check-certificate -O page/23app-sfcmap.html $MICHELLE/page/23app-sfcmap
+
+# wget --no-check-certificate -O page/.html $MICHELLE/page/
+# wget --no-check-certificate -O page/.html $MICHELLE/page/
