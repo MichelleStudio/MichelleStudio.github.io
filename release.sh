1c731c5c8d13e091b73ae8959eb127be6e9e9cbf
realmatic <admin@realmatic.com>
2024-07-04 19:39:29 -0700

    release

--- release.sh
+++ release.sh
@@ -1,34 +1,41 @@
 # set DEBUG false, production ASSET URL 
 
-wget --no-check-certificate -O index.html $MICHELLE 
+#wget --no-check-certificate -O index.html $MICHELLE 
 
-wget --no-check-certificate -O page/work.html $MICHELLE/page/work
-wget --no-check-certificate -O page/process.html $MICHELLE/page/process
-wget --no-check-certificate -O page/biocv.html $MICHELLE/page/biocv
+#wget --no-check-certificate -O page/work.html $MICHELLE/page/work
+#wget --no-check-certificate -O page/process.html $MICHELLE/page/process
+#wget --no-check-certificate -O page/biocv.html $MICHELLE/page/biocv
 
-wget --no-check-certificate -O page/21-12-31-make-Current.html $MICHELLE/page/21-12-31-make-Current 
-wget --no-check-certificate -O page/21-12-31-work-Current.html $MICHELLE/page/21-12-31-work-Current 
-wget --no-check-certificate -O page/22-12-13-make-Vigor.html $MICHELLE/page/22-12-13-make-Vigor 
-wget --no-check-certificate -O page/22-12-13-work-Vigor.html $MICHELLE/page/22-12-13-work-Vigor 
-wget --no-check-certificate -O page/23-03-15-make-Occupation.html $MICHELLE/page/23-03-15-make-Occupation 
-wget --no-check-certificate -O page/23-03-15-work-Occupation.html $MICHELLE/page/23-03-15-work-Occupation 
-wget --no-check-certificate -O page/23-05-01-make-Deliberate.html $MICHELLE/page/23-05-01-make-Deliberate 
-wget --no-check-certificate -O page/23-05-01-work-Deliberate.html $MICHELLE/page/23-05-01-work-Deliberate 
-wget --no-check-certificate -O page/23-12-08-make-Essence.html $MICHELLE/page/23-12-08-make-Essence
-wget --no-check-certificate -O page/23-12-08-work-Essence.html $MICHELLE/page/23-12-08-work-Essence
-wget --no-check-certificate -O page/23-12-18-make-Cranes.html $MICHELLE/page/23-12-18-make-Cranes
+#wget --no-check-certificate -O page/21-12-31-make-Current.html $MICHELLE/page/21-12-31-make-Current 
+#wget --no-check-certificate -O page/21-12-31-work-Current.html $MICHELLE/page/21-12-31-work-Current 
+#wget --no-check-certificate -O page/22-12-13-make-Vigor.html $MICHELLE/page/22-12-13-make-Vigor 
+#wget --no-check-certificate -O page/22-12-13-work-Vigor.html $MICHELLE/page/22-12-13-work-Vigor 
+#wget --no-check-certificate -O page/23-03-15-make-Occupation.html $MICHELLE/page/23-03-15-make-Occupation 
+#wget --no-check-certificate -O page/23-03-15-work-Occupation.html $MICHELLE/page/23-03-15-work-Occupation 
+#wget --no-check-certificate -O page/23-05-01-make-Deliberate.html $MICHELLE/page/23-05-01-make-Deliberate 
+#wget --no-check-certificate -O page/23-05-01-work-Deliberate.html $MICHELLE/page/23-05-01-work-Deliberate 
+#wget --no-check-certificate -O page/23-12-08-make-Essence.html $MICHELLE/page/23-12-08-make-Essence
+#wget --no-check-certificate -O page/23-12-08-work-Essence.html $MICHELLE/page/23-12-08-work-Essence
+#wget --no-check-certificate -O page/23-12-18-make-Cranes.html $MICHELLE/page/23-12-18-make-Cranes
 
-wget --no-check-certificate -O page/23app-sfcmap.html $MICHELLE/page/23app-sfcmap
-wget --no-check-certificate -O page/23-art-app.html $MICHELLE/page/23-art-app
+#wget --no-check-certificate -O page/23app-sfcmap.html $MICHELLE/page/23app-sfcmap
+#wget --no-check-certificate -O page/23-art-app.html $MICHELLE/page/23-art-app
 
-wget --no-check-certificate -O page/desinv_showcase.html $MICHELLE/page/desinv_showcase
-wget --no-check-certificate -O page/desinv_team.html $MICHELLE/page/desinv_team
-wget --no-check-certificate -O page/desinv_document.html $MICHELLE/page/desinv_document
+#wget --no-check-certificate -O page/desinv_showcase.html $MICHELLE/page/desinv_showcase
+#wget --no-check-certificate -O page/desinv_team.html $MICHELLE/page/desinv_team
+#wget --no-check-certificate -O page/desinv_document.html $MICHELLE/page/desinv_document
 
 # new site
 wget --no-check-certificate -O page/newsite.html $MICHELLE
 wget --no-check-certificate -O page/visdev.html $MICHELLE/page/visdev
+wget --no-check-certificate -O page/visdev_art.html $MICHELLE/page/visdev_art
+wget --no-check-certificate -O page/visdev_character.html $MICHELLE/page/visdev_character
+wget --no-check-certificate -O page/visdev_scenic.html $MICHELLE/page/visdev_scenic
 wget --no-check-certificate -O page/visdev_anna.html $MICHELLE/page/visdev_anna
+wget --no-check-certificate -O page/visdev_hega.html $MICHELLE/page/visdev_hega
+wget --no-check-certificate -O page/visdev_cccp.html $MICHELLE/page/visdev_cccp
+wget --no-check-certificate -O page/visdev_daum.html $MICHELLE/page/visdev_daum
+wget --no-check-certificate -O page/visdev_deju.html $MICHELLE/page/visdev_deju_
 wget --no-check-certificate -O page/conservation.html $MICHELLE/page/conservation
 wget --no-check-certificate -O page/conservation_sacr.html $MICHELLE/page/conservation_sacr
 wget --no-check-certificate -O page/conservation_spro.html $MICHELLE/page/conservation_spro
